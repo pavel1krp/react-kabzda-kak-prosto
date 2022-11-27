98f852040206a4a33addaaf1cf3ebc2ec197059d
Pavel <pavel07007@gmail.com>
2022-11-27 23:16:22 +0300

    just try to add randomNumberGenerator for training callback

--- src/components/UncontrolRating.tsx
+++ src/components/UncontrolRating.tsx
@@ -1,38 +1,38 @@
 import React, {useState} from 'react';
 
-type RatingPropstype ={
+type RatingPropstype = {
     // value: 0|1|3|4|5|2;
-    callBack: (num:number)=>void
+    callBack: (num: number) => void
 }
 
-const Rating = ()=> {
-    const [value,setValue] = useState(0)
-    const handler = (num:number)=>{
+const Rating = () => {
+    const [value, setValue] = useState(0)
+    const handler = (num: number) => {
         setValue(num)
     }
     return (
         <div>
-            <Star selected ={value >0}  callBack={handler} id={1}/>
-            <Star selected ={value >1} callBack={handler} id={2}/>
-            <Star selected ={value >2} callBack={handler} id={3}/>
-            <Star selected ={value >3} callBack={handler} id={4}/>
-            <Star selected ={value >4} callBack={handler} id={5}/>
+            <Star selected={value > 0} callBack={handler} id={1}/>
+            <Star selected={value > 1} callBack={handler} id={2}/>
+            <Star selected={value > 2} callBack={handler} id={3}/>
+            <Star selected={value > 3} callBack={handler} id={4}/>
+            <Star selected={value > 4} callBack={handler} id={5}/>
         </div>
     );
 };
 
 export default Rating;
 
-type StarPropsType ={
+type StarPropsType = {
     selected: boolean,
-    callBack:(num:number)=>void
-    id:number;
+    callBack: (num: number) => void
+    id: number;
 }
 
-const Star = (props: StarPropsType)=>{
-    const onClickHandler =()=> {
+const Star = (props: StarPropsType) => {
+    const onClickHandler = () => {
         props.callBack(props.id)
     }
-    return  props.selected === true? <span onClick={onClickHandler}><b>star </b></span>:
- <span onClick={onClickHandler}>star </span>;
+    return props.selected === true ? <span onClick={onClickHandler}><b>star </b></span> :
+        <span onClick={onClickHandler}>star </span>;
 }
